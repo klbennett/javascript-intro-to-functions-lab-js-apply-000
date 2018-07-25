212bf13a75faf97f8eff3588b6341e0fbe9ffbe1
Kay B <kaylouisebennett@gmail.com>
2018-07-25 15:36:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,11 +18,12 @@ function logWhisper(string) {
 var hello = 'hello'
 
 function sayHiToGrandma(string) {
-  if (string === string.toLowerCase)
-  return "I can't hear you!";
-else if (string === string.toLowerCase)
+  if { {string === string.toLowerCase)
+  return "I can't hear you!"; }
+else if { (string === string.toLowerCase)
 return "YES INDEED!";
-else if (string === string.toMixedCase)
+}
+else 
 return "I love you, too.";
 }
 }
