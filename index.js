a04f35903797338ecc479eeed7b399836feef7bc
Kay B <kaylouisebennett@gmail.com>
2018-07-25 15:33:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,5 +22,7 @@ function sayHiToGrandma(string) {
   return "I can't hear you!";
 else if (string === string.toLowerCase)
 return "YES INDEED!";
-
+else if (string === string.toLowerCase)
+return "YES INDEED!";
+}
 }
